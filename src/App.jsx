9f13f40eea9a220ef6423cbb141cd727c8f5998b
Creative-Monk-Team <team.creativemonk@gmail.com>
2024-11-15 12:46:52 +0530

    done basic boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AllRoutes from "./routes/Allroutes";
+import { HelmetProvider } from "react-helmet-async";
+import PopupForm from "./components/PopupForm";
+import WaitingLoader from "./components/WaitingLoader";
 
 function App() {
   return (
     <>
       <div id="auramah_valley">
-        <AllRoutes />
+        <ToastContainer />
+        <HelmetProvider>
+          <AllRoutes />
+        </HelmetProvider>
+        <PopupForm />
+        <WaitingLoader />
       </div>
     </>
   );
